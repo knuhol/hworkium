a08f2e53ea3a6e664aa84f3f555a931e641a7df6
Knut Holm <knut.holm28@gmail.com>
2019-10-23 10:04:23 +0200

    feat: 🎸 Added i18n

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,39 +1,49 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 import NavigationBar from './NavigationBar';
 import { ROUTE } from './routes';
 
-const App: React.FC = () => (
-  <>
-    <Router>
-      <NavigationBar />
-      <Switch>
-        <Route exact path={ROUTE.HOME}>
-          <div></div>
-        </Route>
-        <div className="mt-3">
-          <Container>
-            <Route ecxact path={ROUTE.HELLO_WORLD}>
-              <Row>
-                <Col>
-                  <h1>Hello world!</h1>
-                </Col>
-              </Row>
-            </Route>
-            <Route exact path={ROUTE.TEST}>
-              <Row>
-                <Col>
-                  <h1>Test</h1>
-                </Col>
-              </Row>
-            </Route>
-          </Container>
-        </div>
-      </Switch>
-    </Router>
-  </>
-);
+const App: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route exact path={ROUTE.HOME}>
+            <></>
+          </Route>
+
+          <Route ecxact path={ROUTE.HELLO_WORLD}>
+            <div className="mt-3">
+              <Container>
+                <Row>
+                  <Col>
+                    <h1>Hello world!</h1>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+          </Route>
+          <Route exact path={ROUTE.TEST}>
+            <div className="mt-3">
+              <Container>
+                <Row>
+                  <Col>
+                    <h1>{t('test')}</h1>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import './i18n';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Suspense fallback="Loading...">
+    <App />
+  </Suspense>,
+  document.getElementById('root')
+);
